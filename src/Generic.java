d0471b5d6302ab4cb552065eab7b4a697ed66c99
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-07 19:25:20 +0530

    ADDED : 3 Integer Object and Compare To Method

--- src/Generic.java
+++ src/Generic.java
@@ -1,7 +1,18 @@
 public class Generic {
 
-   public static void main(String[] args){
+    public static <T extends Comparable<T>> T maximum(T a, T b, T c){
+        T max = a;
 
-        System.out.println("Welcome To Generic");
-   }
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("the maximum number is :" + maximum(2, 4, 8));
+
+    }
 }
